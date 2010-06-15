4c091b21f5957f6e218b1ea61a97d6df2a9f4aa4
James Stevenson <james.m.stevenson@gmail.com>
2010-06-14 23:44:20 -0400

    Far, far more complete test coverage.

--- scaffold/tests.py
+++ scaffold/tests.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
 from django.db import models, transaction
 from django.db.models.loading import cache
-from django.template import Context, Template
+from django.template import Context, Template, TemplateDoesNotExist
 from django.test import TestCase
 
 from models import BaseSection
@@ -74,6 +74,16 @@ class SectionTest(TestCase):
     test_user = 'testrunner'
     test_password = 'testrunner'    
     
+    def _patch_get_extending_model(self):
+        # A little patching to make sure that get_extending_model() returns
+        # TestSection.
+        from scaffold import settings
+        def get_test_model():
+            return TestSection
+        settings.get_extending_model = get_test_model
+        from middleware import _build_section_path_map
+        _build_section_path_map()   
+    
     def _disable_csrf_middleware(self):
         settings.MIDDLEWARE_CLASSES = filter(lambda m: 'CsrfMiddleware' \
             not in m, settings.MIDDLEWARE_CLASSES
@@ -128,6 +138,14 @@ class SectionTest(TestCase):
         import admin_views
         admin_views.Section = TestSection # Monkey patch!
     
+    def test_section_view(self):
+        TestSection.load_bulk(BASE_DATA)
+        import views, middleware
+        self._patch_get_extending_model()
+        url = TestSection.objects.get(slug='231').get_absolute_url()
+        result = self.client.get(url)
+        self.assertEqual(result.status_code, 200)
+        
     def test_admin_index(self):
         """
         Verify that each section in the section tree can be found on the admin 
@@ -140,15 +158,42 @@ class SectionTest(TestCase):
 
     def test_admin_section_create_move(self):
         """
-        Via the admin interface, create a new section in the the tree under the 
-        "2" section and then move it to the "4" section as the last child. 
+        Via the admin interface, create a new section in the the tree under 
+        the "2" section and then move it to the "4" section as the last child. 
         """
+        # Try creating a root-level node.
         self.login_and_load()
+        url = reverse("sections:add", kwargs={'section_id': 'root'})
+        response = self.client.post(url, {
+            'slug': 'bazz',
+            'title': 'Bazz',
+            'description': 'Bazz description'
+        })
+        # On sucess should redirect.
+        self.assertRedirects(response, reverse("sections:sections_index"))
+        self.assertTrue('bazz' in \
+            [node.slug for node in TestSection.get_root_nodes()]
+        )
+        # Try creating a node that is a child         
         test_section = TestSection.objects.get(slug="2")
         url = reverse("sections:add", kwargs={'section_id': test_section.id})
         response = self.client.get(url)
         self.assertContains(response, test_section.title)
-        # Create a new section via the form and verify it exists.
+        # Create a new section, but use a bad position:
+        response = self.client.post(url, {
+            'slug': 'foobar',
+            'title': 'Foo Bar',
+            'description': 'Foo Bar description',
+            'position': 'beside',
+            'child': '4'
+        })
+        self.assertEqual(response.status_code, 400)
+        try:
+            section = TestSection.objects.get(slug="foobar")
+            section.delete()
+        except TestSection.DoesNotExist:
+            pass
+        # Create a new section properly via the form and verify it exists.
         response = self.client.post(url, {
             'slug': 'foobar',
             'title': 'Foo Bar',
@@ -164,8 +209,17 @@ class SectionTest(TestCase):
         self.assertEqual(foobar.get_parent(), test_section)
         # ...and make sure it was positioned correctly.
         self.assertEqual(test_section.get_children()[2].pk, foobar.pk)        
-        # Now move the section.
+        # Now move the section...
         url = reverse("sections:move", kwargs={'section_id': foobar.id})
+        response = self.client.get(url)
+        self.assertEqual(response.context['section'].pk, foobar.pk)
+        # ... first with an incorrect reltionship field...
+        response = self.client.post(url, {
+            'relationship': 'sibling', 
+            'to': TestSection.objects.get(slug="4").id
+        })
+        self.assertEqual(response.status_code, 400)
+        # ... then a correct one.
         response = self.client.post(url, {
             'relationship': 'child', 
             'to': TestSection.objects.get(slug="4").id
@@ -177,6 +231,13 @@ class SectionTest(TestCase):
             TestSection.objects.get(slug="foobar").get_parent(),
             TestSection.objects.get(slug="4")
         )
+        # Try moving a section to itself.
+        url = reverse("sections:move", kwargs={'section_id': foobar.id})
+        response = self.client.post(url, {
+            'relationship': 'child', 
+            'to': foobar.id
+        })
+        self.assertEqual(response.status_code, 400)
         # Move section to root of tree.
         response = self.client.post(url, {
             'relationship': 'neighbor', 
@@ -224,7 +285,9 @@ class SectionTest(TestCase):
         url = reverse(
             "sections:edit", 
             kwargs={'section_id': test_section.id}
-        )        
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.context['section'].slug, test_section.slug)      
         response = self.client.post(url, {
             'slug': '41b',
             'title': 'Forty One B',
@@ -237,6 +300,26 @@ class SectionTest(TestCase):
         self.assertTrue(edited_section.title == 'Forty One B')
         self.assertTrue(edited_section.description == 'Description tktk.')
     
+    def test_admin_section_related(self):
+        """View related content via the admin interface."""
+        self.login_and_load()
+        test_section = TestSection.objects.get(slug="41")
+        url = reverse("sections:related_content", 
+            kwargs = {'section_id': test_section.id}
+        )
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 200)
+        
+    def test_admin_section_order_all_content(self):
+        """View related content via the admin interface."""
+        self.login_and_load()
+        test_section = TestSection.objects.get(slug="41")
+        url = reverse("sections:order", 
+            kwargs = {'section_id': test_section.id}
+        )
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 200)
+        
     def test_model_get_related_content(self):
         """Test the BaseSection model's get_related_content method"""
         TestSection.load_bulk(BASE_DATA)  
@@ -326,25 +409,61 @@ class SectionTest(TestCase):
     def test_templatetag_get_root_sections(self):
         """Test that the get_root_sections template tag works as expected."""        
         TestSection.load_bulk(BASE_DATA)
-        # A little patching to make sure that get_extending_model() returns
-        # TestSection.
-        from scaffold import settings as app_settings
-        def get_test_model():
-            return TestSection
-        app_settings.get_extending_model = get_test_model
+        self._patch_get_extending_model()
+        # Test basic version of template tag.
         template = Template("""{%load sections%}
         <ul>{% get_root_sections as root_sections %}
         {{root_sections|pprint}}
+        {%for nav in root_sections%}<li>{{nav.title}}</li>{%endfor%}
+        </ul>    
+        """)
+        context = Context({})
+        result = template.render(context)
+        # Test extended version of template tag.
+        for root in TestSection.get_root_nodes():
+            self.assertTrue(root.title in result)
+        template = Template("""{%load sections%}
+        <ul>{% get_root_sections with subsection as root_sections %}
+        {{root_sections|pprint}}
         {%for nav in root_sections%}
-            <li{%if nav.is_active%} class="active"{%endif%}>
-                <a href="{{nav.get_absolute_url}}">{{nav.title}}</a>
-            </li>
+            <li{%if nav.is_active%} class="active"{%endif%}>{{nav.title}}</li>
         {%endfor%}
         </ul>    
         """)
-        
         context = Context({'subsection': TestSection.objects.get(slug='231')})
         result = template.render(context)
         for root in TestSection.get_root_nodes():
             self.assertTrue(root.title in result)
+        self.assertTrue('<li class="active">2</li>' in result)
+    
+    def test_templatetag_section_is_descendant(self):
+        """
+        Test that the section_is_descendant template tag works as 
+        expected.
+        """        
+        TestSection.load_bulk(BASE_DATA)
+        template = Template("""{%load sections%}
+        {% section_is_descendant subsection of rootsection as descends %}
+        {% if descends%}Passes Test 1{%endif%}
+        {% section_is_descendant orphan of rootsection as descends %}
+        {% if descends%}{%else%}Passes Test 2{%endif%}            
+        """)
+        context = Context({
+            'subsection': TestSection.objects.get(slug='231'),
+            'orphan': TestSection.objects.get(slug='1'),
+            'rootsection': TestSection.objects.get(slug='2'),
+        })
         
+        result = template.render(context)
+        self.assertTrue('Passes Test 1' in result)
+        self.assertTrue('Passes Test 2' in result)
+        
+        # Render with a bad context:
+        context = Context({
+            'subsection': None,
+            'orphan': None,
+            'rootsection': None,
+        })        
+        result = template.render(context)
+        self.assertTrue('Passes Test 1' not in result)
+        self.assertTrue('Passes Test 2' in result)
