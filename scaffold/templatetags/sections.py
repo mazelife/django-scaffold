6dacdf55bd033c99fa7142ba0c109467cedd1a2d
James Stevenson <james.m.stevenson@gmail.com>
2010-09-10 12:48:37 -0700

    PEP8 compliancy fixes

--- scaffold/templatetags/sections.py
+++ scaffold/templatetags/sections.py
@@ -145,7 +145,8 @@ def submit_row(context):
         'show_save_as_new': not is_popup and change and save_as,
         'show_save_and_add_another': context['has_add_permission'] and 
                             not is_popup and (not save_as or context['add']),
-        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'show_save_and_continue': not is_popup and\
+            context['has_change_permission'],
         'is_popup': is_popup,
         'show_save': True
     }
