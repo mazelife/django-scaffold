700c4ab7aa360b01928671d750403b5713ae5e1d
James Stevenson <james.stevenson@threespot.com>
2012-08-14 19:38:56 -0400

    Updates to handle data files in install, bumps up version number by 0.1, updates requirements.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from distutils.core import setup
 
-version = '1.0'
+version = '1.1'
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
@@ -22,6 +22,15 @@ if not root_dir:
     root_dir = '.'
 long_desc = open(root_dir + '/README.rst').read()
 
+package_data = [
+    '*.html',
+    '*.gif',
+    '*.png',
+    '*.jpg',
+    '*.js',
+    '*.css'
+]
+
 setup(
     name='django-scaffold',
     version=version,
@@ -31,12 +40,28 @@ setup(
     license='BSD License',
     packages=['scaffold'],
     package_dir={'scaffold': 'scaffold'},
-    package_data={'scaffold': ['templates/admin/*.html']},
+    package_data={'scaffold': [
+        'templates/scaffold/*.html',
+        'templates/scaffold/admin/*.html',
+        'templates/scaffold/admin/includes/*.html',
+        'static/scaffold/images/jstree/*.*',
+        'static/scaffold/images/jstree/*.*',
+        'static/scaffold/images/jstree/file_icons/*.*',
+        'static/scaffold/images/jstree/jquery/*.*',
+        'static/scaffold/images/jstree/plugins/*.*',
+        'static/scaffold/scripts/*.js',
+        'static/scaffold/scripts/jstree/*.js',
+        'static/scaffold/scripts/jstree/_lib/*.js',
+        'static/scaffold/scripts/jstree/themes/django/*.*',
+        'static/scaffold/styles/*.css',
+        'static/scaffold/styles/jquery.ui.start/*.css',
+        'static/scaffold/styles/jquery.ui.start/images/*.png',
+    ]},
     description=(
         'Reusable application for a generic section/subsection hierarchy'
-        ' in Django 1.3'
+        ' in Django 1.3.x'
     ),
     classifiers=classifiers,
     long_description=long_desc,
-    install_requires=['django>=1.3','django-treebeard>=1.61']
+    install_requires=['django>=1.3.3','django-treebeard>=1.61']
 )
